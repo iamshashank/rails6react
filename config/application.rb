d872d9e658cf35231040e44f9a24ae0f734e39ea
Anshul Kumar <anshul.kumar@daffodilsw.com>
2020-01-19 21:24:18 +0530

    refactor

--- config/application.rb
+++ config/application.rb
@@ -33,11 +33,13 @@ module Rails6react
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
-    config.middleware.insert_before 0, Rack::Cors do
-      allow do
-        origins '*'
-        resource '*', headers: :any, methods: [:get, :post, :options]
+    if Rails.env.development?
+      config.middleware.insert_before 0, Rack::Cors do
+        allow do
+          origins '*'
+          resource '*', headers: :any, methods: [:get, :post, :options]
+        end
       end
-    end     
+    end    
   end
 end
